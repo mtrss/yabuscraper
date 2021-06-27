5f2419c6f268ee6dd8eca6849911006166c9655c
Miguel <pumpkin.cake@protonmail.ch>
2021-06-27 16:11:07 -0300

    🔧 Changed the project to a ECMAScript module

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
     "env": {
-        "commonjs": true,
         "es2021": true,
         "node": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
-        "ecmaVersion": 12
+        "ecmaVersion": 12,
+        "sourceType": "module"
     },
     "rules": {
     }
