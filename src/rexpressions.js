1968936555dc009ac50294a1ef1e5d3d572c40d8
Miguel <pumpkin.cake@protonmail.ch>
2021-06-28 21:07:12 -0300

    ✨ Added more expressions to use in other functions

--- src/rexpressions.js
+++ src/rexpressions.js
@@ -6,6 +6,10 @@ export const videoSDExtract = /(?<=label: "HD", file: ").+?(?=")/;
 export const videoHDExtract = /(?<=label: "SD", file: ").+?(?=")/;
 export const latestUpdated =
   /(?<=<div class="loop-content phpvibe-video-list miau">).+?(?=<h3 class="loop-heading">)/;
+export const searchResult =
+  /(?<=<div class="loop-content phpvibe-video-list miau">).*?(?=<script)/;
+export const searchVideoInfo = /(?<=<div id="video-\d+" class="video">).*?(?=<\/li><li><\/li><\/ul>)/g;
 export const videoThumb = /(?<=<div class="video-thumb( )?">).+?(?=<\/div>)/g;
 export const videoThumbInfo = /(?<=<a).+?(?=\/><span class="vertical-align">)/g;
+export const videoTags = /(?<=<li>).+?(?=\.)/;
 export const customTag = (param) => new RegExp(`(?<=${param}=").+?(?=")`);
