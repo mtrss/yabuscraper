807802a65a027b5e83ee00836ef29e8b43afdd49
Miguel <pumpkin.cake@protonmail.ch>
2021-06-29 19:47:39 -0300

    Added the export for the new function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import videoDetails from "./videoDetails.js";
 import latestUpdated from "./latestUpdated.js";
 import search from "./search.js";
+import grabDetails from "./grabDetails.js";
 
 export {
     videoDetails,
     latestUpdated,
     search,
+    grabDetails
 };
